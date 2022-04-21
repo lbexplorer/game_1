ead50874a689f3b65c07546dbc3a8da6859e020c
L <Lclw4659@163.com>
2022-04-21 22:57:22 +0800

    the next day write a little daily

--- game_main/ball_rule.py
+++ game_main/ball_rule.py
@@ -21,3 +21,47 @@ class Ball(pygame.sprite.Sprite):
         # 如果碰到球拍
         if pygame.sprite.collide_rect(ball, racket_left) or pygame.sprite.collide_rect(ball, racket_right):
             self.direction_x, self.direction_y = -self.direction_x, random.choice([1, -1])
+            self.speed += 1
+            scores = [0, 0]
+            hit_sound.play()
+        # 撞上上侧的墙
+        elif self.rect.top == 0:
+            self.direction_y = 1
+            self.speed += 1
+            scores = [0, 0]
+        # 撞上下侧的墙
+        elif self.rect.top == self.cfg.HEIGHT - self.rect.height:
+            self.direction_y = -1
+            self.speed += 1
+        # 撞上左边的墙
+        elif self.rect.left < 0:
+            self.reset()
+            racket_left.reset()
+            racket_right.reset()
+            scores = [0, 1]
+            goal_sound.play()
+        # 撞上右边的墙
+        elif self.rect.right > self.cfg.WIDTH:
+            self.reset()
+            racket_left.reset()
+            racket_right.reset()
+            scores = [1, 0]
+            goal_sound.play()
+        # 普通情况
+        else:
+            scores = [0, 0]
+        return scores
+
+    """初始化"""
+
+    def reset(self):
+        self.rect.centerx = self.cfg.WIDTH // 2
+        self.rect.centery = random.randrange(self.rect.height // 2, self.cfg.HEIGHT - self.rect.height // 2)
+        self.direction_x = random.choice([1, -1])
+        self.direction_y = random.choice([1, -1])
+        self.speed = 1
+
+    """绑定到屏幕上"""
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
